d6436d6f9eff0f483a39a531c5cc3c396680233e
JosipMarkovic20 <josip.j.markovic@gmail.com>
2019-05-28 22:43:26 +0200

    Networking started

--- app/src/main/java/hr/ferit/brunozoric/taskie/app/TaskieApp.kt
+++ app/src/main/java/hr/ferit/brunozoric/taskie/app/TaskieApp.kt
@@ -2,6 +2,8 @@ package hr.ferit.brunozoric.taskie.app
 
 import android.app.Application
 import android.content.Context
+import android.content.SharedPreferences
+import hr.ferit.brunozoric.taskie.persistence.PREFERENCES_NAME
 
 class TaskieApp : Application() {
 
@@ -9,11 +11,15 @@ class TaskieApp : Application() {
         private lateinit var instance: TaskieApp
 
         fun getAppContext(): Context = instance.applicationContext
+
+        fun providePreferences(): SharedPreferences = TaskieApp.instance.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
     }
 
     override fun onCreate() {
         instance = this
         super.onCreate()
     }
+
+
 
 }
